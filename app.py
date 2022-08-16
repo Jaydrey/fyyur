e85d858035f1809368c2dae0d25f11bfe23d6785
Caleno-R <calenorose@gmail.com>
2022-08-16 19:01:36 +0300

    fixed create show  submission

--- app.py
+++ app.py
@@ -471,20 +471,6 @@ def edit_venue(venue_id):
     form = VenueForm()
     venue = Venue.query.get(venue_id)
 
-    # venue = {
-    #     "id": 1,
-    #     "name": "The Musical Hop",
-    #     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    #     "address": "1015 Folsom Street",
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "123-123-1234",
-    #     "website": "https://www.themusicalhop.com",
-    #     "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    #     "seeking_talent": True,
-    #     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    #     "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    # }
     # TODO: populate form with values from venue with ID <venue_id>
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -614,9 +600,12 @@ def create_show_submission():
         start_time=request.form['start_time'],
     )
     try:
-        Show.insert(new_show)
+        db.session.add(new_show)
+        db.session.commit()
+
     except Exception as e:
         flash(f'An error occurred. Show couldn\'t be created')
+        db.session.rollback()
     # on successful db insert, flash success
     flash('Show was successfully listed!')
     # TODO: on unsuccessful db insert, flash an error instead.
